3757d3ce44b5b2075f331a00ecc6be63d4b68996
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2015-09-03 15:26:57 +0200

    removed unused libraries

--- sha3/sha3_256.py
+++ sha3/sha3_256.py
@@ -1,7 +1,7 @@
-import sys
+# import sys
 from cffi import FFI
-import rlp
-from rlp.utils import decode_hex, encode_hex
+# import rlp
+# from rlp.utils import decode_hex, encode_hex
 
 ffi = FFI()
 lib = ffi.dlopen("/home/czepluch/Dropbox/prgrms/ethereum/pypy_sha3/lib/libsha3.so")
@@ -9,12 +9,16 @@ lib = ffi.dlopen("/home/czepluch/Dropbox/prgrms/ethereum/pypy_sha3/lib/libsha3.s
 ffi.cdef('''
          int sha3_256(uint8_t*, size_t, uint8_t const*, size_t);
          ''')
-seed = ''
-output_length = 32
-inpt = ffi.new("uint8_t[]", str(seed))
-outpt = ffi.new("uint8_t[]", output_length)
-lib.sha3_256(outpt, output_length, inpt, len(seed))
 
-buf = ffi.buffer(outpt, output_length)
-printbuf = buf[:]
+output_length = 32
+outpt = ffi.new("uint8_t[]", output_length)
+
+
+def sha3_256(seed):
+    inpt = ffi.new("uint8_t[]", str(seed))
+    lib.sha3_256(outpt, output_length, inpt, len(seed))
+    buf = ffi.buffer(outpt, output_length)
+    return buf[:]
+
+printbuf = sha3_256('')
 print(printbuf.encode('hex'))
