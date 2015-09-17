db28c8dccc217c8e561e63f390e3d0747db53226
Ulrich Petri <ulo@ulo.pe>
2015-09-17 18:28:49 +0200

    Restructure package to fix setup.py CFFI integration

--- setup.py
+++ setup.py
@@ -1,37 +1,30 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 
-pykeccak = Extension('pykeccak',
-                      sources=['lib/sha3.c'],
-                      depends=['lib/compiler.h', 'lib/sha3.h'],
-                      extra_compile_args=["-Isrc/", "-std=gnu99", "-Wall"]
-                     )
+sha3 = Extension(
+    'keccak.sha3',
+    sources=['lib/sha3.c'],
+    depends=['lib/compiler.h', 'lib/sha3.h'],
+    extra_compile_args=["-Isrc/", "-std=gnu99", "-Wall"]
+)
 
 
-setup(name="pykeccak",
-      version='0.1.9',
-      description="Keccak 256 hashing for PyPy2",
-      author="Jacob Stenum Czepluch",
-      author_email="j.czepluch@gmail.com",
-      url="https://github.com/czepluch/pykeccak",
-      license="MIT",
-      ext_modules=[pykeccak],
-      install_requires=["cffi>=1.2.1"],
-      setup_requires=["cffi>=1.2.1"],
-      classifiers=[
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: Implementation :: PyPy"
-        ],
-      )
-
-# cffi specific setup.
-from cffi import FFI
-ffi = FFI()
-
-ffi.set_source("_keccak", None)
-ffi.cdef('''
-         int sha3_256(uint8_t*, size_t, uint8_t const*, size_t);
-         ''')
-
-if __name__ == '__main__':
-    ffi.compile()
+setup(
+    name="pykeccak",
+    version='0.1.9',
+    description="Keccak 256 hashing for PyPy2",
+    author="Jacob Stenum Czepluch",
+    author_email="j.czepluch@gmail.com",
+    url="https://github.com/czepluch/pykeccak",
+    license="MIT",
+    packages=find_packages(exclude=["_cffi_build", "_cffi_build/*"]),
+    ext_modules=[sha3],
+    install_requires=["cffi>=1.2.1"],
+    setup_requires=["cffi>=1.2.1"],
+    cffi_modules=["_cffi_build/keccak_build.py:ffi"],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: PyPy"
+    ],
+    zip_safe=False
+)
