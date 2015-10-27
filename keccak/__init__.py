82e7c064ac4e151af408851e0c772d21c19c8fbe
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2015-10-27 17:14:47 +0100

    added some documentation

--- keccak/__init__.py
+++ keccak/__init__.py
@@ -16,37 +16,59 @@ lib = ffi.dlopen(obj_name)
 
 
 class sha3_256(object):
+    """
+    Creates a sha3 object using the keccak 256 hashing algorithm
+    Takes an optional string. If no seed is given it will default to an epmty string
+    """
 
     def __init__(self, seed=""):
         self.seed = seed
 
+    # Returns the digest of the strings passed to update
     def digest(self):
         return _sha3_256(self.seed)
 
+    # Returns the digest of the strings passed to update
+    # only containing hexadecimal digits
     def hexdigest(self):
         return _sha3_256(self.seed).encode('hex')
 
+    # Update the hash object with the string arg.
+    # update(a); update(b) is the same as update(a + b)
     def update(self, string):
         self.seed = self.seed + string
 
 
 class sha3_512(object):
+    """
+    Creates a sha3 object using the keccak 512 hashing algorithm
+    Takes an optional string. If no seed is given it will default to an epmty string
+    """
 
     def __init__(self, seed=""):
         self.seed = seed
 
+    # Returns the digest of the strings passed to update
     def digest(self):
         return _sha3_512(self.seed)
 
+    # Returns the digest of the strings passed to update
+    # only containing hexadecimal digits
     def hexdigest(self):
         return _sha3_512(self.seed).encode('hex')
 
+    # Update the hash object with the string arg.
+    # update(a); update(b) is the same as update(a + b)
     def update(self, string):
         self.seed = self.seed + string
 
 
 # SHA3-256 hashing using the Keccak standard
 def _sha3_256(seed):
+    """
+    Takes a string as seed
+    Returns the hashed value of the seed
+    """
     # Length of the output of sha256
     output_length = 32
 
@@ -68,6 +90,10 @@ def _sha3_256(seed):
 
 # SHA3-512 hashing using the Keccak standard
 def _sha3_512(seed):
+    """
+    Takes a string as seed
+    Returns the hashed value of the seed
+    """
     # Length of the output of sha512
     output_length = 64
 
